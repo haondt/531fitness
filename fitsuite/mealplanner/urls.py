85d7ee31aadb9c3f4dae5440a2cf79cd3d65fb2a
Noah <burghardtnoah@gmail.com>
2019-07-17 20:42:28 -0600

    added my first view

--- fitsuite/mealplanner/urls.py
+++ fitsuite/mealplanner/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+
+	# eg /mealplanner/fooditems/5/
+	path('fooditems/<int:fooditem_id>/', views.fooditem, name='fooditem'),
 ]
--- fitsuite/mealplanner/views.py
+++ fitsuite/mealplanner/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def index(request):
 	return HttpResponse('Welcome to Meal Planner')
 
+def fooditem(request, fooditem_id):
+	return HttpResponse('Page for fooditem %s' % fooditem_id)
+
